b205d3338dc5dc6ea226a0e172aa192994e7f23d
jlin1599 <168303849+jlin1599@users.noreply.github.com>
2024-09-10 22:07:24 -0400

    Add files via upload

---
+++ Competition.cpp
@@ -0,0 +1,59 @@
+//Author: Jackie Lin
+//Course: CSCI-135
+//Instructor: Professor Tong Yi
+//Assignement: Project 3 Task D
+
+
+//This program
+
+#include <iostream>
+#include "Competition.hpp"
+#include <cstdlib>
+using namespace std;
+
+Competition::Competition() : rabbit(), tor(), lane() {
+    // rabbit with default constructor of Hare class
+    // tor with default constructor of Tortoise class
+    // lane with default constructor of Road class
+}
+
+Competition::Competition(Hare coney, Tortoise cooter, int length)
+    : rabbit(coney), tor(cooter), lane(length) {
+    // rabbit with coney
+    // tor with cooter
+    // lane with length
+}
+void Competition::play() {
+    int round = 1;
+    int lastBlock = lane.length() - 1;
+
+    while(rabbit.getPosition() < lastBlock && tor.getPosition() < lastBlock){
+        rabbit.move();
+        rabbit.setPosition(max(rabbit.getPosition(),0));
+        rabbit.setPosition(min(rabbit.getPosition(),lastBlock));
+        lane.mark(rabbit.getPosition(), 'H');
+
+        tor.move();
+        tor.setPosition(max(tor.getPosition(),0));
+        tor.setPosition(min(tor.getPosition(),lastBlock));
+        lane.mark(tor.getPosition(), 'T');
+
+        cout << "Round " << round << ": ";
+        lane.display();
+
+        if(rabbit.getPosition() == tor.getPosition()){
+            cout << "Ouch! Tortoise bites hare." << endl;
+        }
+        lane.mark(rabbit.getPosition(), ' ');
+        lane.mark(tor.getPosition(), ' ');
+        round++;
+    }
+    if(rabbit.getPosition() == lastBlock){
+        cout << " Yuck, hare wins. " << endl;
+    }else if(tor.getPosition() == lastBlock){
+        cout << "Tortoise wins!" << endl;
+    }else{
+        cout << "It is a tie." << endl;
+    }
+    
+}
